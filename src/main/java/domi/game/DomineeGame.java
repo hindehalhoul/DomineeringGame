e252383527a2133aea12145827f8e037e95b107c
amina <aminabakkali2001@gmail.com>
2023-11-22 23:06:44 +0100

    dominos vert + horz init

--- src/main/java/domi/game/DomineeGame.java
+++ src/main/java/domi/game/DomineeGame.java
@@ -1,34 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package domi.game;
-import java.io.*;
-import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.*;
 
-/**
- *
- * @author ASUS
- */
 public class DomineeGame {
-    
-        private JPanel chessboardPanel; // Added member variable
+    private JPanel chessboardPanel;
+    private Point currentPoint;
+    private boolean dominoPlaced;
+    private List<Domino> placedDominos;
 
-        
-        public DomineeGame() {
-        initializeChessboardPanel(); // Initialize chessboardPanel in the constructor
+    public DomineeGame() {
+        initializeChessboardPanel();
+        placedDominos = new ArrayList<>();
     }
 
     public JPanel getChessboardPanel() {
         return chessboardPanel;
     }
 
-    
     private void initializeChessboardPanel() {
         chessboardPanel = new JPanel() {
             @Override
             public void paint(Graphics g) {
+                super.paint(g);
                 boolean white = true;
                 for (int y = 0; y < 8; y++) {
                     for (int x = 0; x < 8; x++) {
@@ -42,11 +38,74 @@ public class DomineeGame {
                     }
                     white = !white;
                 }
+
+                // Draw the placed dominos
+                for (Domino domino : placedDominos) {
+                    g.setColor(domino.getColor());
+                    if (domino.isHorizontal()) {
+                        g.fillRect(domino.getX() * 64, domino.getY() * 64, 128, 64);
+                    } else {
+                        g.fillRect(domino.getX() * 64, domino.getY() * 64, 64, 128);
+                    }
+                }
+
+                // Draw the domino at the current mouse position with 50% opacity
+                if (currentPoint != null && !dominoPlaced && currentPoint.y < 7) {
+                    g.setColor(new Color(0, 0, 255, 127)); // 50% opacity blue
+                    g.fillRect(currentPoint.x * 64, currentPoint.y * 64, 64, 128);
+                }
+
+                // Draw the domino at the current mouse position with 100% opacity
+                if (currentPoint != null && dominoPlaced && currentPoint.y < 7) {
+                    g.setColor(new Color(0, 0, 255)); // 100% opacity blue
+                    g.fillRect(currentPoint.x * 64, currentPoint.y * 64, 64, 128);
+                }
             }
         };
+
+        // Add a MouseMotionListener to handle mouse movement
+        chessboardPanel.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                int x = e.getX() / 64;
+                int y = e.getY() / 64;
+
+                // Ensure the currentPoint is within the bounds of the board
+                currentPoint = new Point(Math.min(Math.max(x, 0), 7), Math.min(Math.max(y, 0), 7));
+
+                chessboardPanel.repaint();
+            }
+        });
+
+        // Add a MouseListener to handle mouse clicks
+        chessboardPanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (currentPoint.y < 7 && !dominoPlaced) {
+                    // Place the vertical domino
+                    placedDominos.add(new Domino(currentPoint.x, currentPoint.y, false, new Color(0, 0, 255)));
+                    dominoPlaced = true;
+                    chessboardPanel.repaint();
+                } else if (currentPoint.y < 7 && dominoPlaced && !isHorizontalDominoAt(currentPoint.x, currentPoint.y)) {
+                    // Place the horizontal domino if there is no horizontal domino at the current location
+                    placedDominos.add(new Domino(currentPoint.x, currentPoint.y, true, new Color(128, 0, 128)));
+                    dominoPlaced = false;
+                    chessboardPanel.repaint();
+                }
+            }
+        });
     }
-    
-     public static void main(String[] args) {
+
+    private boolean isHorizontalDominoAt(int x, int y) {
+        for (Domino domino : placedDominos) {
+            if (domino.isHorizontal() && domino.getX() == x && domino.getY() == y) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setBounds(10, 10, 512, 512);
         frame.setUndecorated(true);
@@ -57,5 +116,34 @@ public class DomineeGame {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
-    
+}
+
+class Domino {
+    private int x;
+    private int y;
+    private boolean horizontal;
+    private Color color;
+
+    public Domino(int x, int y, boolean horizontal, Color color) {
+        this.x = x;
+        this.y = y;
+        this.horizontal = horizontal;
+        this.color = color;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isHorizontal() {
+        return horizontal;
+    }
+
+    public Color getColor() {
+        return color;
+    }
 }
--- src/main/java/domineering.java
+++ src/main/java/domineering.java
@@ -12,29 +12,38 @@ import javax.swing.*;
  *
  * @author ASUS
  */
+
 public class domineering extends javax.swing.JFrame {
+    private DomineeGame domineeGame;
 
-        private DomineeGame domineeGame; // Instance of DomineeGame
-
-    /**
-     * Creates new form domineering
-     */
     public domineering() {
         initComponents();
-        domineeGame = new DomineeGame(); // Initialize DomineeGame
-        displayChessboard(); // Display the chessboard
+        domineeGame = new DomineeGame();
+        displayChessboard();
     }
-    
+
     private void displayChessboard() {
-        // Get the content pane of the frame
         Container contentPane = getContentPane();
-
-        // Set layout manager for the content pane
         contentPane.setLayout(new BorderLayout());
-
-        // Add the JPanel from DomineeGame to the content pane
         contentPane.add(domineeGame.getChessboardPanel(), BorderLayout.CENTER);
     }
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new domineering().setVisible(true);
+            }
+        });
+    }
+
+
+
+
+
+
+
+
+
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -243,37 +252,7 @@ public class domineering extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(domineering.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(domineering.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(domineering.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(domineering.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new domineering().setVisible(true);
-            }
-        });
-    }
+   
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
